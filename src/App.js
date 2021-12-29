0bff210ac22a79a7cfa27eecdd6712c206a32441
twcrone <twcrone@gmail.com>
2021-12-29 10:54:59 -0500

    Start fetching data from API

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import './App.css';
 import Header from "./Header"
+import {useEffect, useMemo, useState} from "react";
 
 function App() {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    const fetchList = async () => {
+      const rsp = await fetch("https://list4u-api.herokuapp.com/");
+      const items = await rsp.json();
+      setItems(items);
+    };
+    fetchList();
+  }, []);
+
+  const selectedItem = useMemo(() => {
+    if(items.length) {
+      return items[0];
+    }
+  }, [items]);
+
   return (
       <div className="container">
         <Header subtitle="List 4 U" />
